2900898eccf3bf377cd82cdcc6df68252d7a0d36
Aliwoto <aminnimaj@gmail.com>
2021-10-14 19:05:40 +0000

    fix problems in urls
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- scp/plugins/user/reporting.py
+++ scp/plugins/user/reporting.py
@@ -6,7 +6,7 @@ async def report_error(e, module, the_user):
     await user.send_message(user.log_channel,
             f"**{escapeAny('Failed due to: ')}** " +
             f"`{escapeAny(e)}` \n " +
-            f"{escapeAny('by user:')} {escapeAny(the_user)}" +
+            f"{escapeAny('by user:')}\n{escapeAny(the_user)}\n" +
             f"{escapeAny('in module: ')} {escapeAny(module)}")
 
 
