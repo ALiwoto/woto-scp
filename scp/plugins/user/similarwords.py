a3c4e6f78790fe51e8ca0d185787a2654e8c86ee
Aliwoto <aminnimaj@gmail.com>
2021-10-15 09:20:03 +0000

    fix ud problems
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

---
+++ scp/plugins/user/similarwords.py
@@ -0,0 +1,160 @@
+import httpx
+from urllib.parse import quote_plus as quote
+
+def get_similar_words(query: str) -> list:
+	"""
+	Returns a list of similar words to the given query.
+	"""
+
+	if (not query) or (len(query) < 3):
+		return []
+	
+	the_url = ("https://www.google.com/complete/" +
+		"search?q=" + query +
+		"&cp=0&client=gws-wiz" +
+		"&xssi=t&gs_ri=gws-wiz&hl=en-US&authuser=0&" +
+		"pq=" + query +
+		"&psi=qUycYJ_XLseNkwXX7oGgBg.1620855979375&nolsbt=1&dpr=1")
+	query = quote(query.lower().strip())
+	with httpx.Client() as ses:
+		resp = ses.get(the_url)
+		return parse_google_data(resp.content.decode("utf-8"))
+
+
+async def get_similar_words_async(query: str) -> list:
+	"""
+	Returns a list of similar words to the given query.
+	"""
+
+	if (not query) or (len(query) < 3):
+		return []
+	
+	the_url = ("https://www.google.com/complete/" +
+		"search?q=" + query +
+		"&cp=0&client=gws-wiz" +
+		"&xssi=t&gs_ri=gws-wiz&hl=en-US&authuser=0&" +
+		"pq=" + query +
+		"&psi=qUycYJ_XLseNkwXX7oGgBg.1620855979375&nolsbt=1&dpr=1")
+	query = quote(query.lower().strip())
+	async with httpx.AsyncClient() as ses:
+		resp = await ses.get(the_url)
+		return parse_google_data(resp.content.decode("utf-8"))
+
+
+def parse_google_data(gStr: str) -> list:
+	"""
+	Parses the data returned response from the Google API.
+	"""
+
+	if (not gStr) or (len(gStr) < 2):
+		return []
+	
+	final = ""
+	allStrs = []
+	firstChild = False
+	secondChild = False
+	thirdChild = False
+	insideStr = False
+	childDone = False
+	additionalChild = 0
+	
+	gStr = gStr.replace("\\u003cb\\u003e", "").replace("\\u003c\\/b\\u003e", "")
+	for current in gStr:
+		if current != '[':
+			if current == '"':
+				# ensure that we are in correct time line, Steins;Gate.
+				if not thirdChild:
+					#Forbidden!
+					# we are only allowed to do operations in
+					# third child, not in any other childs!
+					continue
+				
+				if not childDone:
+					if insideStr:
+						insideStr = False
+						childDone = True
+					else:
+						insideStr = True
+					continue
+				else:
+					continue
+			
+			# CHAR_S8  = ']'
+			if current == ']':
+				# ensure that we have already passed the first child.
+				# actually I don't know what the hell are these characters
+				# at the first of recieved data from google: )]}'.
+				# really, what's wrong with them? maybe it's only me
+				# whose response is like this.
+				# anyway, it won't hurt if I write a checker here.
+				if not firstChild:
+					continue
+					
+				# we should check if this ended child was
+				# an official child or not.
+				if additionalChild == 0:
+					# it means this is an official child.
+
+					# we should check if this ended child
+					# is the third child or not.
+					if thirdChild:
+						# it means third child has been ended.
+						# now our duty is to append it to the
+						# allStrs array.
+						allStrs.append(final)
+						final = ""
+						childDone = False
+						thirdChild = False
+						continue
+
+					if secondChild:
+						# if the result is not found in google's server,
+						# we will get a response like this:
+						# [[],{"i":"rellowrellow","q":"kikrhTfn3q01Ssg4sD_3SYeA8B4"}]
+						if len(allStrs) == 0:
+							return None
+
+						# we are done! return all of the slices.
+						return allStrs
+					
+					# no need for check for first child,
+					# since we don't know if its contents is
+					# bullshit or not.
+					# for more information, compare
+					# example09.json and example08.json with each other.
+					# if firstChild {
+					#
+					#}
+					continue
+				else:
+					additionalChild -= 1
+
+			if insideStr:
+				final += current
+			
+			continue
+		else:
+			# check if it's our first child or not.
+			if not firstChild:
+				firstChild = True
+				continue
+
+			# check if it's our second child or not.
+			if not secondChild:
+				secondChild = True
+				continue
+
+			# check if it's our third child or not.
+			if not thirdChild:
+				thirdChild = True
+				continue
+			
+			# we have at most 3 official children.
+			# we need only these children.
+			# but in the data, we will have more than these.
+			# we don't know how many, maybe 4, maybe 5 or even 6.
+			# which is why we have to use counter for them.
+			additionalChild += 1
+	
+	return allStrs
+
--- scp/plugins/user/urbandictionary.py
+++ scp/plugins/user/urbandictionary.py
@@ -1,4 +1,6 @@
 from scp import user
+from similarwords import get_similar_words_async
+from utils.mdparser import escapeAny
 
 
 __PLUGIN__ = 'UrbanDictionary'
@@ -26,6 +28,12 @@ def replace_text(text: str):
         '\\', '',
     )
 
+def is_undefined(response) -> bool:
+    return ((not response) or (not response['list']) or (len(response['list']) == 0)
+        or (len(response['list'][0] == 0)) or 
+        (len(response['list'][0]['word'] == 0)) or 
+        (len(response['list'][0]['definition'] == 0)))
+
 
 @user.on_message(
     (user.sudo | user.owner) & 
@@ -38,25 +46,39 @@ async def _(_, message: user.types.Message):
         f'http://api.urbandictionary.com/v0/define?term={text}',
         type='get',
     )
-    text = user.md.KanTeXDocument(
-        user.md.Section(
-            'UrbanDictionary',
-            user.md.SubSection(
-                'Text:',
-                user.md.Italic(replace_text(response['list'][0]['word'])),
-            ),
-            user.md.SubSection(
-                'Meaning:',
-                user.md.Italic(
-                    replace_text(
-                        response['list'][0]['definition'],
+    text = ""
+    if is_undefined(response):
+        similars = await get_similar_words_async(text)
+        if (not similars) or (len(similars) == 0):
+            text = escapeAny(f'No definition found for "{text}"...')
+        else:
+            text = escapeAny(f'No definition found for "{text}"...\n'+
+                'but here are some similarities:')
+            num = 0
+            for similar in similars:
+                num += 1
+                text += escapeAny(f'\n{num}- {similar}')
+        return await message.reply(text, parse_mode="MarkdownV2")
+    else:    
+        text = user.md.KanTeXDocument(
+            user.md.Section(
+                'UrbanDictionary',
+                user.md.SubSection(
+                    'Text:',
+                    user.md.Italic(replace_text(response['list'][0]['word'])),
+                ),
+                user.md.SubSection(
+                    'Meaning:',
+                    user.md.Italic(
+                        replace_text(
+                            response['list'][0]['definition'],
+                        ),
                     ),
                 ),
+                user.md.SubSection(
+                    'Example:',
+                    user.md.Italic(replace_text(response['list'][0]['example'])),
+                ),
             ),
-            user.md.SubSection(
-                'Example:',
-                user.md.Italic(replace_text(response['list'][0]['example'])),
-            ),
-        ),
-    )
+        )
     await message.reply(text, quote=True)
