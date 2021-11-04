e0ec40c2254f51f5be02b412cd2f5d0770b7fac3
aliwoto <aminnimaj@gmail.com>
2021-11-04 17:56:29 -0400

    Fix problems in sibylUtils
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/plugins/user/sibyl.py
+++ scp/plugins/user/sibyl.py
@@ -23,8 +23,10 @@ async def _(_, message: user.types.Message):
     try:
         the_info = user.sibyl.get_info(get_user)
         if the_info.banned:
-            await message.reply_text("This user is banned.", parse_mode="HTML", disable_web_page_preview=True)
+            await message.reply_text(html.escape("This user is banned."), parse_mode="HTML", disable_web_page_preview=True)
             return
+        else:
+            await message.reply_text(html.escape(f"{the_info}"), parse_mode="HTML", disable_web_page_preview=True)
     except Exception as e:
         await message.reply_text("<code>" + html.escape(str(e)) + "</code>", parse_mode="HTML")
         return
