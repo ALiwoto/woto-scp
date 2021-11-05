83052669767b7df51872eb06010e7732b9f621ec
aliwoto <aminnimaj@gmail.com>
2021-11-05 08:13:33 -0400

    Update sibylUtils.py
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/utils/sibylUtils.py
+++ scp/utils/sibylUtils.py
@@ -1,109 +1,11 @@
 from pyrogram.types.user_and_chats.user import User
 from sibylsystem.SibylSystem import PsychoPass
 from sibylsystem.SibylSystem.types import (
-    BanResult as SibylBanResult,
-    Ban as SibylBanInfo,
+    Ban,
 )
 from typing import List, Dict, Any, TypeVar, Callable, Type, cast
 from sibylsystem.SibylSystem.exceptions import GeneralException
-
-class UserInfo:
-    user_id: int
-    banned: bool
-    reason: str
-    message: str
-    ban_source_url: str
-    banned_by: int
-    crime_coefficient: int
-    date: str
-    ban_flags: List[str]
-
-    def __init__(self, user_id: int, banned: bool, reason: str, message: str, ban_source_url: str, banned_by: int, crime_coefficient: int, date: str, ban_flags: List[str]) -> None:
-        self.user_id = user_id
-        self.banned = banned
-        self.reason = reason
-        self.message = message
-        self.ban_source_url = ban_source_url
-        self.banned_by = banned_by
-        self.crime_coefficient = crime_coefficient
-        self.date = date
-        self.ban_flags = ban_flags
-
-    @staticmethod
-    def from_dict(obj: Any) -> 'UserInfo':
-        assert isinstance(obj, dict)
-        user_id = from_int(obj.get("user_id"))
-        banned = from_bool(obj.get("banned"))
-        reason = from_str(obj.get("reason"))
-        message = from_str(obj.get("message"))
-        ban_source_url = from_str(obj.get("ban_source_url"))
-        banned_by = from_int(obj.get("banned_by"))
-        crime_coefficient = from_int(obj.get("crime_coefficient"))
-        date = from_str(obj.get("date"))
-        ban_flags = from_list(from_str, obj.get("ban_flags"))
-        return UserInfo(user_id, banned, reason, message, ban_source_url, banned_by, crime_coefficient, date, ban_flags)
-
-    def to_dict(self) -> dict:
-        result: dict = {}
-        result["user_id"] = from_int(self.user_id)
-        result["banned"] = from_bool(self.banned)
-        result["reason"] = from_str(self.reason)
-        result["message"] = from_str(self.message)
-        result["ban_source_url"] = from_str(self.ban_source_url)
-        result["banned_by"] = from_int(self.banned_by)
-        result["crime_coefficient"] = from_int(self.crime_coefficient)
-        result["date"] = from_str(self.date)
-        result["ban_flags"] = from_list(from_str, self.ban_flags)
-        return result
-
-
-class UserBannedResult:
-    previous_ban: UserInfo
-    current_ban: UserInfo
-
-    def __init__(self, previous_ban: UserInfo, current_ban: UserInfo) -> None:
-        self.previous_ban = previous_ban
-        self.current_ban = current_ban
-
-    @staticmethod
-    def from_dict(obj: Any) -> 'UserBannedResult':
-        assert isinstance(obj, dict)
-        previous_ban = UserInfo.from_dict(obj.get("previous_ban"))
-        current_ban = UserInfo.from_dict(obj.get("current_ban"))
-        return UserBannedResult(previous_ban, current_ban)
-
-    def to_dict(self) -> dict:
-        result: dict = {}
-        result["previous_ban"] = to_class(UserInfo, self.previous_ban)
-        result["current_ban"] = to_class(UserInfo, self.current_ban)
-        return result
-
-
-class BanResponse:
-    success: bool
-    result: UserBannedResult
-    error: None
-
-    def __init__(self, success: bool, result: UserBannedResult, error: None) -> None:
-        self.success = success
-        self.result = result
-        self.error = error
-
-    @staticmethod
-    def from_dict(obj: Any) -> 'BanResponse':
-        assert isinstance(obj, dict)
-        success = from_bool(obj.get("success"))
-        result = UserBannedResult.from_dict(obj.get("result"))
-        error = from_none(obj.get("error"))
-        return BanResponse(success, result, error)
-
-    def to_dict(self) -> dict:
-        result: dict = {}
-        result["success"] = from_bool(self.success)
-        result["result"] = to_class(UserBannedResult, self.result)
-        result["error"] = from_none(self.error)
-        return result
-
+from sibylsystem.SibylSystem.types.bans import BanRes
 
 class StatsResult:
     banned_count: int
@@ -236,80 +138,33 @@ class StatsResponse:
         return result
 
 
-
-
-class UserInfoResponse:
-    success: bool
-    result: UserInfo
-    error: None
-
-    def __init__(self, success: bool, result: UserInfo, error: None) -> None:
-        self.success = success
-        self.result = result
-        self.error = error
-
-    @staticmethod
-    def from_dict(obj: Any) -> 'UserInfoResponse':
-        assert isinstance(obj, dict)
-        success = from_bool(obj.get("success"))
-        result = UserInfo.from_dict(obj.get("result"))
-        error = from_none(obj.get("error"))
-        return UserInfoResponse(success, result, error)
-
-    def to_dict(self) -> dict:
-        result: dict = {}
-        result["success"] = from_bool(self.success)
-        result["result"] = to_class(UserInfo, self.result)
-        result["error"] = from_none(self.error)
-        return result
-
-
-def UserInfoResponse_from_dict(s: Any) -> UserInfoResponse:
-    return UserInfoResponse.from_dict(s)
-
-
-
 def stats_from_dict(s: Any) -> StatsResponse:
     return StatsResponse.from_dict(s)
 
-
-
-def BanResponse_from_dict(s: Any) -> BanResponse:
-    return BanResponse.from_dict(s)
 
 class SibylClient(PsychoPass):
     def __init__(self, token: str):
         super().__init__(token, show_license=False)
 
     def revert(self, user_id: int) -> bool:
-        resp = self.invoke_request(f"{self.host}removeBan?token={self.token}&user-id={user_id}")
-        if self.is_success(resp):
-            return resp['result']
-        raise GeneralException(resp['error']['message'])
+        raise self.delete_ban(user_id)
     
     def revert_ban(self, user_id: int) -> bool:
-        return self.revert(user_id)
+        return self.delete_ban(user_id)
     
     def remove_ban(self, user_id: int) -> bool:
-        return self.revert(user_id)
+        return self.delete_ban(user_id)
     
     def clear_ban(self, user_id: int) -> bool:
-        return self.revert(user_id)
+        return self.delete_ban(user_id)
     
-    def user_info_old(self, user_id: int) -> UserInfo:
-        resp = self.invoke_request(f"{self.host}getInfo?token={self.token}&user-id={user_id}")
-        if not self.is_success(resp):
-            raise GeneralException(resp["error"]["message"])
-        the_resp = UserInfoResponse_from_dict(resp)
-        return the_resp.result
-    
-    def user_info(self, user_id: int) -> SibylBanInfo:
+    def user_info(self, user_id: int) -> Ban:
         return self.get_info(user_id)
     
-    def ban(self, user_id: int, reason: str, message: str=None, source: str=None) -> SibylBanResult:
+    def ban(self, user_id: int, reason: str, message: str=None, source: str=None) -> BanRes:
         return self.add_ban(user_id, reason, message, source)
     
-    def ban_user(self, user_id: int, reason: str, message: str=None, source: str=None) -> UserBannedResult:
+    def ban_user(self, user_id: int, reason: str, message: str=None, source: str=None) -> BanRes:
         return self.add_ban(user_id, reason, message, source)
 
     def stats(self) -> StatsResult:
@@ -326,6 +181,7 @@ class SibylClient(PsychoPass):
         self.host = host
     
     def change_perm(self, user_id: int, permission: int) -> str:
+        self.change_permission
         resp = self.invoke_request((f"{self.host}changePerm?token={self.token}"+
             f"&user-id={user_id}&permission={permission}"))
         if not self.is_success(resp):
