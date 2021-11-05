6706206591872e05a53ae19930ec59d5b241a0c7
aliwoto <aminnimaj@gmail.com>
2021-11-05 18:11:16 -0400

    Update sibyl.py
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/plugins/user/sibyl.py
+++ scp/plugins/user/sibyl.py
@@ -8,7 +8,7 @@ import html
     (user.sudo | user.owner) &
     user.command('sinfo'),
 )
-async def _(_, message: user.types.Message):
+async def sinfo_handler(_, message: user.types.Message):
     cmd = message.command
     if not message.reply_to_message and len(cmd) == 1:
         get_user = message.from_user.id
@@ -57,4 +57,59 @@ async def _(_, message: user.types.Message):
     except Exception as e:
         await my_msg.edit_text("Got error: <code>" + html.escape(str(e)) + "</code>", parse_mode="HTML")
         return
+
+@user.on_message(
+    (user.sudo | user.owner) &
+    user.command('sban'),
+)
+async def sban_handler(_, message: user.types.Message):
+    cmd = message.command
+    if not message.reply_to_message and len(cmd) == 1:
+        get_user = message.from_user.id
+    elif len(cmd) == 1:
+        if message.reply_to_message.forward_from:
+            get_user = message.reply_to_message.forward_from.id
+        else:
+            get_user = message.reply_to_message.from_user.id
+    elif len(cmd) > 1:
+        get_user = cmd[1]
+        try:
+            get_user = int(cmd[1])
+        except ValueError:
+            pass
+    ptxt = "Sending cymatic scan request to Sibyl System."
+    my_msg = await message.reply_text(ptxt)
+    sleep(1.2)
+    ptxt += "."
+    my_msg = await my_msg.edit_text(ptxt)
+    sleep(1.2)
+    ptxt += "."
+    my_msg = await my_msg.edit_text(ptxt)
+    try:
+        the_info = user.sibyl.user_info(get_user)
+        if not the_info:
+            await my_msg.edit_text('failed to receive info from Sibyl System.')
+            return
+        txt = "<b>" + "Sibyl System scan results:" + "</b>\n"
+        txt += "<b>" + "‍ • ID: " + "</b><code>" + str(the_info.user_id) + "</code>\n"
+        txt += "<b>" + "‍ • Is banned: " + "</b><code>" + str(the_info.banned) + "</code>\n"
+        if the_info.banned:
+            if the_info.banned_by != 0:
+                txt += "<b>" + "‍ • Banned by: " + "</b><code>" + str(the_info.banned_by) + "</code>\n"
+            if the_info.ban_flags and len(the_info.ban_flags) > 0:
+                f = ', '.join(the_info.ban_flags)
+                txt += "<b>" + "‍ • Ban flags: " + "</b><code>" + html.escape(f) + "</code>\n"
+            txt += "<b>" + "‍ • Crime Coefficient: " + "</b><code>" + str(the_info.crime_coefficient) + "</code>\n"
+            txt += "<b>" + "‍ • Last update: " + "</b><code>" + str(the_info.date) + "</code>\n"
+            txt += "<b>" + "‍ • Ban reason: " + "</b><code>" + html.escape(the_info.reason) + "</code>\n"
+        else:
+            txt += "<b>" + "‍ • Crime Coefficient: " + "</b><code>" + str(the_info.crime_coefficient) + "</code>\n"
+            txt += "<b>" + "‍ • Last update: " + "</b><code>" + str(the_info.date) + "</code>\n"
+        
+        
+        await my_msg.edit_text(txt, parse_mode="HTML", disable_web_page_preview=True)
+    except Exception as e:
+        await my_msg.edit_text("Got error: <code>" + html.escape(str(e)) + "</code>", parse_mode="HTML")
+        return
+
 
