1951a8e16553bf9865a9138f0e608c4ae37b13bd
aliwoto <aminnimaj@gmail.com>
2021-11-17 13:07:25 +0000

    Add auto-read
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

---
+++ scp/plugins/user/autoRead.py
@@ -0,0 +1,43 @@
+from scp import user
+
+__PLUGIN__ = 'autoread'
+
+@user.on_message(
+    ~(user.owner | user.sudo),
+)
+async def auto_read_handler(_, message: user.types.Message):
+    if not user.auto_read_enabled:
+        return
+    try:
+        await user.read_history(message.chat.id)
+    except Exception:
+        return
+    return
+
+@user.on_message(
+    user.owner & user.command('gautoread'),
+)
+async def sinfo_handler(_, message: user.types.Message):
+    if message.text.find('true') > 0:
+        if user.auto_read_enabled:
+            await message.reply_text('Auto read is already enabled.')
+            return
+        user.auto_read_enabled = True
+        await message.reply_text('Auto read has been enabled.')
+        
+        return
+
+    if message.text.find('false') > 0:
+        if not user.auto_read_enabled:
+            await message.reply_text('Auto read is already disabled.')
+            return
+        user.auto_read_enabled = False
+        await message.reply_text('Auto read has been disabled.')
+        
+        return
+
+    if user.auto_read_enabled:
+        await message.reply_text('Auto read is enabled.')
+    else:
+        await message.reply_text('Auto read is disabled.')
+    return
