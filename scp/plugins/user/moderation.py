1de909a18ecb67dee7c98f6da6909e46481ec0bc
aliwoto <aminnimaj@gmail.com>
2021-11-26 17:21:47 -0500

    Add moderation module
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

---
+++ scp/plugins/user/moderation.py
@@ -0,0 +1,57 @@
+import asyncio
+import os
+import html
+import sys
+import time
+import shutil
+from datetime import timedelta
+from scp import user
+
+
+
+
+@user.on_message(~user.filters.scheduled & 
+	~user.filters.forwarded & 
+	~user.filters.sticker & 
+	~user.filters.via_bot & 
+	~user.filters.edited & 
+	user.owner & 
+	user.filters.command(
+        ['admins'],
+        prefixes=user.cmd_prefixes,
+    ))
+async def admins_handler(_, message: user.types.Message):
+    all_strs = message.text.split(' ')
+    if len(all_strs) < 2:
+        the_chat = message.chat.id
+    else:
+        the_chat = message.text.split(' ')[1]
+        
+    top_msg = await message.reply_text(f"<code>{html.escape('fetching group admins...')}</code>")
+    txt = ''
+    m = await user.get_chat_members(the_chat)
+    creator = None
+    admins = []
+    for i in m:
+        if i.status == 'creator':
+            creator = i
+        elif i.status == 'administrator':
+            admins.append(i)
+
+    if not creator and len(admins) == 0:
+        top_msg.edit_text(text="Seems like all admins are anon...")
+        return
+
+    starter = "<code>" + " • " + "</code>"
+    if creator:
+        txt += "<bold>" + html.escape("The creator:\n") + "</bold>"
+        txt += starter + f"<a href=tg://user?id={creator.user.id}>{html.escape(creator.user.first_name)}</a>"
+        txt += "\n\n"
+
+    if len(admins) > 0:
+        txt += "<bold>" + html.escape("Admins:\n") + "</bold>"
+        for admin in admins:
+            txt += starter + f"<a href=tg://user?id={admin.user.id}>{html.escape(admin.user.first_name)}</a>"
+            txt += "\n"
+    
+    top_msg.edit_text(text=txt, parse_mode="html")
