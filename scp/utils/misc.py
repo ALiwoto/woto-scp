82201b4207126a322f4cbed41e6ba9167d3cd6f0
aliwoto <aminnimaj@gmail.com>
2021-11-27 13:51:12 -0500

    Fix stuff in members command
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/utils/misc.py
+++ scp/utils/misc.py
@@ -2,9 +2,11 @@ import os
 import sys
 import signal
 import psutil
-from pyrogram.types import InlineKeyboardButton
+from pyrogram import types
 
-class _KB(InlineKeyboardButton):
+from scp.utils.strUtils import remove_invisible
+
+class _KB(types.InlineKeyboardButton):
     def __eq__(self, other):
         return self.text == other.text
 
@@ -54,6 +56,75 @@ def paginate_modules(_page_n, module_dict, prefix, chat=None):
         pairs.append((modules[-1],))
     return pairs
 
+def can_user_match(user: types.User, query: str) -> bool:
+    query = remove_special_chars(query)
+    if not user or len(query) < 2:
+        return False
+
+    if len(user.first_name) > 0 and can_str_param_match(user.first_name, query):
+        return True
+    
+    if len(user.last_name) > 0 and can_str_param_match(user.last_name, query):
+        return True
+    
+    if len(user.username) > 0 and can_str_param_match(user.username, query):
+        return True
+    
+    if len(user.status) > 0 and can_str_param_match(user.username, query):
+        return True
+
+    return False
+
+
+def can_member_match(m: types.ChatMember, query: str) -> bool:
+    user = m.user
+    query = remove_special_chars(query)
+    if not user or len(query) < 2:
+        return False
+
+    if len(user.first_name) > 0 and can_str_param_match(user.first_name, query):
+        return True
+    
+    if len(user.last_name) > 0 and can_str_param_match(user.last_name, query):
+        return True
+    
+    if len(user.username) > 0 and can_str_param_match(user.username, query):
+        return True
+    
+    if len(user.status) > 0 and can_str_param_match(user.username, query):
+        return True
+    
+    if len(m.title) > 0 and can_str_param_match(user.username, query):
+        return True
+
+    return False
+
+
+
+
+def can_str_param_match(param: str, query: str) -> bool:
+    return remove_special_chars(param).find(query) != -1
+
+def remove_special_chars(value: str) -> str:
+    value = remove_invisible(value.lower().strip())
+    last_space = False
+    result = ''
+    for current in value:
+        o = ord(value)
+        if current == " ":
+            if last_space:
+                continue
+            result += " "
+            continue
+        elif last_space:
+            last_space = False
+        if current.isspace() or o < 65:
+            continue
+        if o > 122 and o < 250:
+            continue
+        result += value
+    
+    return result
 
 def restart_scp(update_req: bool = False, hard: bool = False) -> None:
         """ Restart the woto-scp """
--- scp/utils/strUtils.py
+++ scp/utils/strUtils.py
@@ -26,10 +26,30 @@ def permissionParser(perms):
     text += 'Pin: ' + bool_check(perms.can_pin_messages) + '\n'
     return text
 
-def replace_text(text):
+def replace_text(text: str) -> str:
+    if not isinstance(text, str):
+        return ''
     return text.replace('"', "").replace("\\r", "").replace("\\n", "").replace("\\", "")
 
 def remove_prefix(text: str, prefix: str) -> str:
     if text.startswith(prefix):
         text = text.replace(prefix, "", 1)
     return text
+
+
+# removes invisible characters from a text so we can match it correctly.
+def remove_invisible(value: str) -> str:
+    #from \u2060 to \u2069
+    #from \u200C to \u200F; \u061C
+    result = ""
+    ocurrent = 0x0
+    for current in value:
+        ocurrent = ord(current)
+        if ocurrent >= 0x2060 and ocurrent <= 0x2069:
+            continue
+        elif ocurrent >= 0x200C and ocurrent <= 0x200F:
+            continue
+        elif ocurrent == 0x061C:
+            continue
+        result += current
+    return result
