5388d8c4f2c6a379e61539196d9b8e3226396af3
aliwoto <aminnimaj@gmail.com>
2021-11-27 14:59:41 -0500

    Fix stuff in members command
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/plugins/user/moderation.py
+++ scp/plugins/user/moderation.py
@@ -1,4 +1,6 @@
 import asyncio
+
+from pyrogram.methods.chats.get_chat_members import Filters
 from scp import user
 from scp.utils.misc import can_member_match
 from scp.utils.parser import (
@@ -39,11 +41,11 @@ async def admins_handler(_, message: user.types.Message):
     txt = ''
     m = None
     try:
-        m = await user.get_chat_members(the_chat)
+        m = await user.get_chat_members(the_chat, filter=Filters.ADMINISTRATORS)
     except Exception as ex:
         await top_msg.edit_text(text=html_mono(ex))
         return
-    creator = None
+    creator: user.types.ChatMember = None
     admins: list[user.types.ChatMember] = []
     bots: list[user.types.ChatMember] = []
     for i in m:
@@ -168,7 +170,7 @@ async def members_handler(_, message: user.types.Message):
         ['fmembers'],
         prefixes=user.cmd_prefixes,
     ))
-async def members_handler(_, message: user.types.Message):
+async def fmembers_handler(_, message: user.types.Message):
     all_strs = split_all(message.text, ' ', '\n', '\t')
     is_here = False
     query = ""
@@ -205,7 +207,7 @@ async def members_handler(_, message: user.types.Message):
     bots: list[user.types.ChatMember] = []
     for i in m:
         if i.status == 'member' and i.status != 'left' and i.status != 'kicked':
-            if not can_member_match(i.user, query):
+            if not can_member_match(i, query):
                 continue
             if i.user.is_bot:
                 bots.append(i)
@@ -236,4 +238,101 @@ async def members_handler(_, message: user.types.Message):
         return
 
     await top_msg.edit_text(text=txt, parse_mode="html")
+
+
+
+
+@user.on_message(~user.filters.scheduled & 
+	~user.filters.forwarded & 
+	~user.filters.sticker & 
+	~user.filters.via_bot & 
+	~user.filters.edited & 
+	user.owner & 
+	user.filters.command(
+        ['fadmins'],
+        prefixes=user.cmd_prefixes,
+    ))
+async def fadmins_handler(_, message: user.types.Message):
+    all_strs = split_all(message.text, ' ', '\n', '\t')
+    is_here = False
+    query = ""
+    if len(all_strs) < 2:
+        the_chat = message.chat.id
+        is_here = True
+    else:
+        the_chat = message.text.split(' ')[1]
+        if the_chat.find('/') > 0:
+            all_strs = the_chat.split('/')
+            index = len(all_strs) - 1
+            if all_strs[index].isdigit():
+                index -= 1
+            if all_strs[index].isdigit():
+                all_strs[index] = '-100' + all_strs[index]
+            the_chat = all_strs[index]
+    
+    if len(all_strs) >= 3:
+        query = ''.join(all_strs[2:])
+    elif is_here and len(all_strs) == 2:
+        query = all_strs[1]
+    else: return
+        
+    top_msg = await message.reply_text(html_mono('fetching group admins...'))
+    txt: str = ''
+    m = None
+    try:
+        m = await user.get_chat_members(the_chat, filter=Filters.ADMINISTRATORS)
+    except Exception as ex:
+        await top_msg.edit_text(text=html_mono(ex))
+        return
+    
+    creator: user.types.ChatMember = None
+    admins: list[user.types.ChatMember] = []
+    bots: list[user.types.ChatMember] = []
+    for i in m:
+        if i.status == 'creator':
+            if not can_member_match(i, query):
+                continue
+            creator = i
+            continue
+        elif i.status == 'administrator':
+            if not can_member_match(i, query):
+                continue
+            if i.user.is_bot:
+                bots.append(i)
+                continue
+            admins.append(i)
+
+    if not creator and len(admins) == 0 and len(bots) == 0:
+        await top_msg.edit_text(text="No results found...")
+        return
+
+    starter = "<code>" + " • " + "</code>"
+    starter = "<code>" + " • " + "</code>"
+    if creator:
+        txt += html_bold("The creator:", "\n")
+        u = creator.user
+        txt += starter + mention_user_html(u, 16) + await html_in_common(u) + html_mono(u.id)
+        txt += "\n\n"
+    
+    if len(admins) > 0:
+        txt += html_bold("Admins:", "\n")
+        for member in admins:
+            u = member.user
+            txt += starter + mention_user_html(u, 16) + ": " + html_mono(u.id, "\n")
+        txt += "\n"
+    
+    if len(bots) > 0:
+        txt += html_bold("Bots:", "\n")
+        for bot in bots:
+            u = bot.user
+            txt += starter + mention_user_html(u, 16) + ": " + html_mono(u.id, "\n")
+        txt += "\n"
+    
+    if len(txt) > 4096:
+        await asyncio.gather(top_msg.delete(), message.reply_document(to_output_file(txt)))
+        return
+
+    await top_msg.edit_text(text=txt, parse_mode="html")
+
+
 
