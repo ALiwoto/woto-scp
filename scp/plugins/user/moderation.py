c98572c99ff2444c6d7e492d139fb9bb39b95fa7
aliwoto <aminnimaj@gmail.com>
2021-11-27 11:13:09 -0500

    Add members command
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/plugins/user/moderation.py
+++ scp/plugins/user/moderation.py
@@ -2,6 +2,7 @@ import asyncio
 import html
 from io import BytesIO
 from scp import user
+from scp.utils.parser import fix_encoding, mention_user_html, to_output_file
 
 @user.on_message(~user.filters.scheduled & 
 	~user.filters.forwarded & 
@@ -55,15 +56,17 @@ async def admins_handler(_, message: user.types.Message):
     starter = "<code>" + " • " + "</code>"
     if creator:
         txt += "<bold>" + html.escape("The creator:") + "</bold>\n"
-        txt += starter + f"<a href=tg://user?id={creator.user.id}>{html.escape(creator.user.first_name[:16])}</a>"
-        txt += f": <code>{creator.user.id}</code>"
+        u = creator.user
+        txt += starter + mention_user_html(u, 16)
+        txt += f": <code>{u.id}</code>"
         txt += "\n\n"
 
     if len(admins) > 0:
         txt += "<bold>" + html.escape("Admins:\n") + "</bold>"
         for admin in admins:
-            txt += starter + f"<a href=tg://user?id={admin.user.id}>{html.escape(admin.user.first_name[:16])}</a>"
-            txt += f": <code>{admin.user.id}</code>"
+            u = admin.user
+            txt += starter + mention_user_html(u, 16)
+            txt += f": <code>{u.id}</code>"
             txt += "\n"
         txt += "\n"
     
@@ -82,4 +85,80 @@ async def admins_handler(_, message: user.types.Message):
         return
 
     await top_msg.edit_text(text=txt, parse_mode="html")
+
+
+
+@user.on_message(~user.filters.scheduled & 
+	~user.filters.forwarded & 
+	~user.filters.sticker & 
+	~user.filters.via_bot & 
+	~user.filters.edited & 
+	user.owner & 
+	user.filters.command(
+        ['members'],
+        prefixes=user.cmd_prefixes,
+    ))
+async def members_handler(_, message: user.types.Message):
+    all_strs = message.text.split(' ')
+    if len(all_strs) < 2:
+        the_chat = message.chat.id
+    else:
+        the_chat = message.text.split(' ')[1]
+        if the_chat.find('/') > 0:
+            all_strs = the_chat.split('/')
+            index = len(all_strs) - 1
+            if all_strs[index].isdigit():
+                index -= 1
+            if all_strs[index].isdigit():
+                all_strs[index] = '-100' + all_strs[index]
+            the_chat = all_strs[index]
+        
+    top_msg = await message.reply_text(f"<code>{html.escape('fetching group members...')}</code>")
+    txt: str = ''
+    m = None
+    try:
+        m = await user.get_chat_members(the_chat)
+    except Exception as ex:
+        await top_msg.edit_text(text=f"<code>{ex}</code>")
+        return
     
+    members: list[user.types.ChatMember] = []
+    bots: list[user.types.ChatMember] = []
+    for i in m:
+        if i.status == 'member' and i.status != 'left' and i.status != 'kicked':
+            if i.user.is_bot:
+                bots.append(i)
+                continue
+            members.append(i)
+
+    if  len(members) == 0 and len(bots) == 0:
+        await top_msg.edit_text(text="Seems like this group doesn't have any normal members...")
+        return
+
+    starter = "<code>" + " • " + "</code>"
+    if len(members) > 0:
+        txt += "<bold>" + html.escape("Admins:\n") + "</bold>"
+        for member in members:
+            u = member.user
+            txt += starter + mention_user_html(u, 16)
+            txt += f": <code>{u.id}</code>"
+            txt += "\n"
+        txt += "\n"
+    
+    if len(bots) > 0:
+        txt += "<bold>" + html.escape("Bots:\n") + "</bold>"
+        for bot in bots:
+            u = bot.user
+            txt += starter + mention_user_html(u, 16)
+            txt += f": <code>{u.id}</code>"
+            txt += "\n"
+        txt += "\n"
+    
+    if len(txt) > 4096:
+        await asyncio.gather(top_msg.delete(), message.reply_document(to_output_file(txt)))
+        return
+
+    await top_msg.edit_text(text=txt, parse_mode="html")
+
+
+
