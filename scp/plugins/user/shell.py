a0db717681cf0dce7533f1f671999e42b34732dd
aliwoto <aminnimaj@gmail.com>
2021-12-09 17:27:15 +0000

    Add some stuff
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/plugins/user/shell.py
+++ scp/plugins/user/shell.py
@@ -4,7 +4,6 @@ import asyncio
 from io import BytesIO
 from scp import user
 
-
 @user.on_message(
     ~user.filters.forwarded
     & ~user.filters.sticker
@@ -17,8 +16,9 @@ from scp import user
     ),
 )
 async def shell(_, message: user.types.Message):
-    command = message.text.split(None, 1)[1]
-    await shell_base(message, command)
+    if isinstance(message, user.types.Message):
+        command = message.text.split(None, 1)[1]
+        await shell_base(message, command)
 
 
 @user.on_message(
@@ -63,9 +63,16 @@ async def git_handler(_, message: user.types.Message):
     ),
 )
 async def screen_handler(_, message: user.types.Message):
-    await shell_base(message, "screen -ls" if message.text == "screen" else message.text[1:])
+    if isinstance(message, user.types.Message):
+        await shell_base(
+            message, 
+            "screen -ls" if message.text[1:].lower() == "screen" else message.text[1:],
+        )
 
 async def shell_base(message: user.types.Message, command: str):
+    if not isinstance(message, user.types.Message):
+        return
+    
     reply = await message.reply_text('Executing...', quote=True)
     process = await asyncio.create_subprocess_shell(
         command,
@@ -73,6 +80,7 @@ async def shell_base(message: user.types.Message, command: str):
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.STDOUT,
     )
+
     stdout, _ = await process.communicate()
     returncode = process.returncode
     doc = user.md.KanTeXDocument()
@@ -96,5 +104,51 @@ async def shell_base(message: user.types.Message, command: str):
         await reply.edit_text(doc)
 
 
+import os
+import html
+import tempfile
+from pyrogram import Client, filters
+from .. import config, help_dict, log_errors, session, progress_callback, public_log_errors
 
+@Client.on_message(~filters.scheduled & ~filters.forwarded & ~filters.sticker & ~filters.via_bot & ~filters.edited & filters.me & filters.command('cat', prefixes=config['config']['prefixes']))
+@log_errors
+@public_log_errors
+async def cat(client, message):
+    media = (message.text or message.caption).markdown.split(' ', 1)[1:]
+    if media:
+        media = os.path.expanduser(media[0])
+    else:
+        media = message.document
+        if not media and not getattr(message.reply_to_message, 'empty', True):
+            media = message.reply_to_message.document
+        if not media:
+            await message.reply_text('Document or local file path required')
+            return
+    done = False
+    reply = rfile = None
+    try:
+        if not isinstance(media, str):
+            rfile = tempfile.NamedTemporaryFile()
+            reply = await message.reply_text('Downloading...')
+            await client.download_media(media, file_name=rfile.name, progress=progress_callback, progress_args=(reply, 'Downloading...', False))
+            media = rfile.name
+        with open(media, 'r') as file:
+            while True:
+                chunk = file.read(4096)
+                if not chunk:
+                    break
+                if not chunk.strip():
+                    continue
+                chunk = f'<code>{html.escape(chunk)}</code>'
+                if done:
+                    await message.reply_text(chunk, quote=False)
+                else:
+                    await getattr(reply, 'edit_text', message.reply_text)(chunk)
+                    done = True
+    finally:
+        if rfile:
+            rfile.close()
+
+help_dict['cat'] = ('cat', '''{prefix}cat <i>(as caption of text file or reply)</i> - Outputs file's text to Telegram
+{prefix}cat <i>&lt;path to local file&gt;</i> - Outputs file's text to Telegram''')
 
