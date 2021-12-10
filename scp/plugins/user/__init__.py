aae9189d0dc6636bd8a694ffd37ed82160723800
ALiwoto <aminnimaj@gmail.com>
2021-12-10 09:40:34 +0000

    Fix some stuff
    Signed-off-by: ALiwoto <aminnimaj@gmail.com>

--- scp/plugins/user/__init__.py
+++ scp/plugins/user/__init__.py
@@ -1,110 +1,13 @@
-
-import time
-import asyncio
-import mimetypes
-from datetime import timedelta
-
+from os.path import dirname, basename, isfile
+import glob
 
 def __list_all_modules():
-    from os.path import dirname, basename, isfile
-    import glob
     mod_paths = glob.glob(dirname(__file__) + '/*.py')
     return [
         basename(f)[:-3]
         for f in mod_paths
         if isfile(f) and f.endswith('.py') and not f.endswith('__init__.py')
     ]
-
-
-# https://stackoverflow.com/a/49361727
-def format_bytes(size):
-    size = int(size)
-    # 2**10 = 1024
-    power = 1024
-    n = 0
-    power_labels = {0 : '', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
-    while size > power:
-        size /= power
-        n += 1
-    return f"{size:.2f} {power_labels[n]+'B'}"
-
-# https://stackoverflow.com/a/34325723
-def return_progress_string(current, total):
-    filled_length = int(30 * current // total)
-    return '[' + '=' * filled_length + ' ' * (30 - filled_length) + ']'
-
-# https://stackoverflow.com/a/852718
-# https://stackoverflow.com/a/775095
-def calculate_eta(current, total, start_time):
-    if not current:
-        return '00:00:00'
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    seconds = (elapsed_time * (total / current)) - elapsed_time
-    thing = ''.join(str(timedelta(seconds=seconds)).split('.')[:-1]).split(', ')
-    thing[-1] = thing[-1].rjust(8, '0')
-    return ', '.join(thing)
-
-progress_callback_data = dict()
-async def progress_callback(current, total, reply, text, upload):
-    message_identifier = (reply.chat.id, reply.message_id)
-    last_edit_time, prevtext, start_time = progress_callback_data.get(message_identifier, (0, None, time.time()))
-    if current == total:
-        try:
-            progress_callback_data.pop(message_identifier)
-        except KeyError:
-            pass
-    elif (time.time() - last_edit_time) > 1:
-        handle = 'Upload' if upload else 'Download'
-        if last_edit_time:
-            speed = format_bytes((total - current) / (time.time() - start_time))
-        else:
-            speed = '0 B'
-        text = f'''{text}
-<code>{return_progress_string(current, total)}</code>
-
-<b>Total Size:</b> {format_bytes(total)}
-<b>{handle}ed Size:</b> {format_bytes(current)}
-<b>{handle} Speed:</b> {speed}/s
-<b>ETA:</b> {calculate_eta(current, total, start_time)}'''
-        if prevtext != text:
-            await reply.edit_text(text)
-            prevtext = text
-            last_edit_time = time.time()
-            progress_callback_data[message_identifier] = last_edit_time, prevtext, start_time
-
-async def get_file_mimetype(filename):
-    mimetype = mimetypes.guess_type(filename)[0]
-    if not mimetype:
-        proc = await asyncio.create_subprocess_exec(
-            'file',
-            '--brief', 
-            '--mime-type', 
-            filename, 
-            stdout=asyncio.subprocess.PIPE,
-        )
-
-        stdout, _ = await proc.communicate()
-        mimetype = stdout.decode().strip()
-    return mimetype or ''
-
-async def get_file_ext(filename):
-    proc = await asyncio.create_subprocess_exec(
-        'file', 
-        '--brief', 
-        '--extension', 
-        filename, 
-        stdout=asyncio.subprocess.PIPE,
-    )
-
-    stdout, _ = await proc.communicate()
-    ext = stdout.decode().strip().split('/', maxsplit=1)[0]
-    if not ext or ext == '???':
-        mimetype = await get_file_mimetype(filename)
-        ext = mimetypes.guess_extension(mimetype) or '.bin'
-    if not ext.startswith('.'):
-        ext = '.' + ext
-    return ext
 
 
 ALL_MODULES = sorted(__list_all_modules())
--- scp/plugins/user/files.py
+++ scp/plugins/user/files.py
@@ -2,9 +2,8 @@ import asyncio
 import os
 import html
 import sys
-import time
 import shutil
-from datetime import timedelta
+from scp.utils import progress_callback
 from scp import user
 
 @user.on_message(~user.filters.scheduled & 
@@ -161,67 +160,6 @@ async def download(_, message: user.types.Message):
         await message.reply_text('Download cancelled!')
     else:
         await reply.edit_text(f'Downloaded to {html.escape(file)}')
-
-
-
-progress_callback_data = dict()
-async def progress_callback(current: int, total: int, reply: user.types.Message, text: str, upload: bool):
-    message_identifier = (reply.chat.id, reply.message_id)
-    last_edit_time, prevtext, start_time = progress_callback_data.get(message_identifier, (0, None, time.time()))
-    if current == total:
-        try:
-            progress_callback_data.pop(message_identifier)
-        except KeyError:
-            pass
-    elif (time.time() - last_edit_time) > 1:
-        handle = 'Upload' if upload else 'Download'
-        if last_edit_time:
-            speed = format_bytes((total - current) / (time.time() - start_time))
-        else:
-            speed = '0 B'
-        text = (f"{text}\n<code>{return_progress_string(current, total)}</code>"+
-        f"\n<b>Total Size:</b> {format_bytes(total)}" +
-        f"\n<b>{handle}ed Size:</b> {format_bytes(current)}" +
-        f"\n<b>{handle} Speed:</b> {speed}/s" +
-        f"\n<b>ETA:</b> {calculate_eta(current, total, start_time)}")
-        if prevtext != text:
-            await reply.edit_text(text)
-            prevtext = text
-            last_edit_time = time.time()
-            progress_callback_data[message_identifier] = last_edit_time, prevtext, start_time
-
-
-# https://stackoverflow.com/a/34325723
-def return_progress_string(current: int, total: int) -> str:
-    filled_length = int(30 * current // total)
-    return '[' + '=' * filled_length + ' ' * (30 - filled_length) + ']'
-
-
-# https://stackoverflow.com/a/852718
-# https://stackoverflow.com/a/775095
-def calculate_eta(current, total, start_time):
-    if not current:
-        return '00:00:00'
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    seconds = (elapsed_time * (total / current)) - elapsed_time
-    thing = ''.join(str(timedelta(seconds=seconds)).split('.')[:-1]).split(', ')
-    thing[-1] = thing[-1].rjust(8, '0')
-    return ', '.join(thing)
-
-
-
-# https://stackoverflow.com/a/49361727
-def format_bytes(size):
-    size = int(size)
-    # 2**10 = 1024
-    power = 1024
-    n = 0
-    power_labels = {0 : '', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
-    while size > power:
-        size /= power
-        n += 1
-    return f"{size:.2f} {power_labels[n]+'B'}"
 
 
 
