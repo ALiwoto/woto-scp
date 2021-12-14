d59da37bac52d83e8a5a5be6089dc260e03ad391
aliwoto <aminnimaj@gmail.com>
2021-12-14 12:04:47 +0000

    fix some stuff
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/plugins/user/sibyl.py
+++ scp/plugins/user/sibyl.py
@@ -75,7 +75,8 @@ async def sinfo_handler(_, message: Message):
         
         await my_msg.edit_text(txt, parse_mode="HTML", disable_web_page_preview=True)
     except Exception as e:
-        await my_msg.edit_text("Got error: <code>" + html.escape(str(e)) + "</code>", parse_mode="HTML")
+        print(e.__traceback__)
+        await my_msg.edit_text("Got error: " +html_mono(str(e)), parse_mode="HTML")
         return
 
 
