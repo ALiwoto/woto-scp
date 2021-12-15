6fcee8d2754d6bb989777da4d1f391f7a58c69b0
aliwoto <aminnimaj@gmail.com>
2021-12-15 16:12:06 +0000

    Fix some stuff
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/plugins/user/sibyl.py
+++ scp/plugins/user/sibyl.py
@@ -13,6 +13,7 @@ from scp.utils.parser import(
     html_mention_by_user, 
     html_mono, 
     html_normal,
+    split_some,
 )
 
 @user.on_message(
@@ -48,7 +49,7 @@ async def sinfo_handler(_, message: Message):
         ptxt += "."
         my_msg = await my_msg.edit_text(ptxt)
     
-    my_user : User
+    my_user : User = None
     try:
         my_user = await user.get_users(the_user)
     except Exception: pass
@@ -69,30 +70,30 @@ async def sinfo_handler(_, message: Message):
                 value=my_user,
                 client=user,
             )
-            txt += html_bold("‍ • User: ") + the_mention + "\n"
+            txt += html_bold(" • User: ") + the_mention + "\n"
         
-        txt += html_bold("‍ • ID: ") + html_mono(the_info.user_id, "\n")
-        txt += html_bold("‍ • Is banned: ") + html_mono(the_info.banned, "\n")
+        txt += html_bold(" • ID: ") + html_mono(the_info.user_id, "\n")
+        txt += html_bold(" • Is banned: ") + html_mono(the_info.banned, "\n")
         if the_info.banned:
             if the_info.banned_by != 0:
                 the_banner: User
                 try:
                     the_banner = await user.get_users(the_info.banned_by)
                 except Exception: pass
-                txt += html_bold("‍ • Banned by: ") + (
+                txt += html_bold(" • Banned by: ") + (
                     html_mono(the_info.banned_by, "\n") 
                     if not the_banner 
                     else html_mention_by_user(the_banner, "\n")
                 )
             if the_info.ban_flags and len(the_info.ban_flags) > 0:
                 f = ', '.join(the_info.ban_flags)
-                txt += html_bold("‍ • Ban flags: ") + html_mono(f, "\n")
-            txt += html_bold("‍ • Crime Coefficient: ") + html_mono(the_info.crime_coefficient, "\n")
-            txt += html_bold("‍ • Last update: ") + html_mono(the_info.date, "\n")
-            txt += html_bold("‍ • Ban reason: ")+ html_mono(the_info.reason, "\n")
+                txt += html_bold(" • Ban flags: ") + html_mono(f, "\n")
+            txt += html_bold(" • Crime Coefficient: ") + html_mono(the_info.crime_coefficient, "\n")
+            txt += html_bold(" • Last update: ") + html_mono(the_info.date, "\n")
+            txt += html_bold(" • Ban reason: ")+ html_mono(the_info.reason, "\n")
         else:
-            txt += html_bold("‍ • Crime Coefficient: ") + html_mono(the_info.crime_coefficient, "\n")
-            txt += html_bold("‍ • Last update: ") + html_mono(the_info.date, "\n")
+            txt += html_bold(" • Crime Coefficient: ") + html_mono(the_info.crime_coefficient, "\n")
+            txt += html_bold(" • Last update: ") + html_mono(the_info.date, "\n")
             if general_info:
                 div = user.sibyl.get_div(general_info)
                 txt += html_normal(
@@ -109,61 +110,30 @@ async def sinfo_handler(_, message: Message):
         return
 
 
-
 @user.on_message(
     (user.owner) &
     user.command(
-        ['scan', 'scan!']
+        ['fScan', 'fScan!']
     ),
 )
 async def scan_handler(_, message: Message):
-    cmd = message.command
-    if not message.reply_to_message and len(cmd) == 1:
-        the_user = message.from_user.id
-    elif len(cmd) == 1:
-        if message.reply_to_message.forward_from:
-            the_user = message.reply_to_message.forward_from.id
-        else:
-            the_user = message.reply_to_message.from_user.id
-    elif len(cmd) > 1:
-        the_user = cmd[1]
-        try:
-            the_user = int(cmd[1])
-        except ValueError:
-            pass
+    if not message.reply_to_message: return # TODO
+    #cmd = message.command
+    #is_silent = user.is_silent(message)
+    replied = message.reply_to_message
+    target_user = replied.from_user.id
+    the_reason = split_some(message.text, 2, ' ', '\n')
+
     ptxt = "Sending cymatic scan request to Sibyl System."
     my_msg = await message.reply_text(ptxt)
-    sleep(1.2)
-    ptxt += "."
-    my_msg = await my_msg.edit_text(ptxt)
-    sleep(1.2)
-    ptxt += "."
-    my_msg = await my_msg.edit_text(ptxt)
+
     try:
-        the_info = user.sibyl.user_info(the_user)
-        if not the_info:
-            await my_msg.edit_text('failed to receive info from Sibyl System.')
-            return
-        txt = html_bold("Sibyl System scan results:") 
-        txt += html_bold( "‍ • ID: ") + html_mono(the_info.user_id, "\n")
-        txt += html_bold("‍ • Is banned: ") + html_mono(the_info.banned, "\n")
-        if the_info.banned:
-            if the_info.banned_by != 0:
-                txt += html_bold("‍ • Banned by: ") + html_mono(the_info.banned_by, "\n")
-            if the_info.ban_flags and len(the_info.ban_flags) > 0:
-                f = ', '.join(the_info.ban_flags)
-                txt += html_bold("‍ • Ban flags: ") + html_mono(f)
-            txt +=  html_bold("‍ • Crime Coefficient: ") + html_mono(the_info.crime_coefficient, "\n")
-            txt += html_bold(" • Last update: ") + html_mono(the_info.date, "\n")
-            txt += html_bold("‍ • Ban reason: ") + html_mono(the_info.reason, "\n")
-        else:
-            txt +=  html_bold("‍ • Crime Coefficient: ") + html_mono(the_info.crime_coefficient, "\n")
-            txt += html_bold("‍ • Last update: ") + html_mono(the_info.date, "\n")
-        
-        
-        await my_msg.edit_text(txt, parse_mode="HTML", disable_web_page_preview=True)
+        user.sibyl.ban(
+            user_id=target_user,
+            reason=the_reason,
+            source=message.link,
+            message=replied.text,
+        )
     except Exception as e:
         await my_msg.edit_text("Got error: " + html_mono(e), parse_mode="HTML")
-        return
-
 
