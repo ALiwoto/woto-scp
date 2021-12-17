c9d000784b30aeffe1f6fc5cf7547583821cd65a
aliwoto <aminnimaj@gmail.com>
2021-12-17 04:14:15 +0000

    Fix some stuff
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/plugins/user/moderation.py
+++ scp/plugins/user/moderation.py
@@ -238,7 +238,7 @@ async def bots_handler(_, message: Message):
         await top_msg.edit_text(text="Seems like this group doesn't have any bots...")
         return
 
-    txt += '▫ Bots list in ' + html_normal_chat_link(the_group.title, the_group, "\n\n")
+    txt += '▫ Bots list in ' + await html_normal_chat_link(the_group.title, the_group, "\n\n")
     if len(admin_bots) > 0:
         txt += html_bold(f"Admin bots: ({len(admin_bots)})", "\n")
         for member in admin_bots:
