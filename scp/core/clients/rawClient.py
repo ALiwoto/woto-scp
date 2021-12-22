b86730dc701ae856c0ae109d3cd72dbb13f63866
aliwoto <aminnimaj@gmail.com>
2021-12-22 05:43:23 +0000

    Add wotobase
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/core/clients/rawClient.py
+++ scp/core/clients/rawClient.py
@@ -9,25 +9,18 @@ from pyrogram import(
 )
 from scp.core.filters.Command import command
 from scp.utils import wfilters
-from scp.utils.parser import(
-    html_bold,
-    html_link, 
-    html_mention, 
-    html_mono,
-    html_normal_chat_link, 
-    split_some,
-)
 from scp.utils.sibylUtils import SibylClient
 from scp.utils.misc import restart_scp as restart_woto_scp
 from configparser import ConfigParser
 from kantex import md as Markdown
 from aiohttp import ClientSession, client_exceptions
+from .wotobase import WotoClientBase
 import asyncio
 import logging
 
 session.Session.notice_displayed = True
 
-class ScpClient(Client):
+class ScpClient(WotoClientBase):
     def __init__(
         self,
         name: str,
@@ -68,35 +61,6 @@ class ScpClient(Client):
     def command(self, *args, **kwargs):
         return command(*args, **kwargs)
     
-    def is_silent(self, message: types.Message) -> bool:
-        return (
-            isinstance(message, types.Message)
-            and message.command != None
-            and len(message.command) > 0
-            and message.command[0x0].endswith('!')
-        )
-    
-    def get_non_cmd(self, message: types.Message) -> str:
-        my_strs = split_some(message.text, 1, ' ', '\n')
-        if len(my_strs) < 1:
-            return ''
-        return my_strs[1]
-    
-    def html_mono(self, value, *argv) -> str:
-        return html_mono(value, *argv)
-    
-    def html_bold(self, value, *argv) -> str:
-        return html_bold(value, *argv)
-    
-    def html_link(self, value, link: str, *argv) -> str:
-        return html_link(value, link, *argv)
-    
-    async def html_normal_chat_link(self, value, chat: types.Chat, *argv) -> str:
-        return await html_normal_chat_link(value, chat, *argv)
-
-    async def html_mention(self, value: Union[types.User, int], name: str = None, *argv) -> str:
-        return await html_mention(value, name, self, *argv)
-
     async def send(
         self,
         data: raw.core.TLObject,
@@ -127,30 +91,6 @@ class ScpClient(Client):
                 sleep_threshold=sleep_threshold,
             )
     
-    async def get_online_counts(self, chat_id: Union[int, str]) -> int:
-        response = await self.send(
-            self.raw.functions.messages.GetOnlines(
-                peer=await self.resolve_peer(chat_id),
-            )
-        )
-        return getattr(response, 'onlines', 0)
-    
-    async def get_online_count(self, chat_id: Union[int, str]) -> int:
-        return await self.get_online_counts(chat_id)
-    async def get_onlines_count(self, chat_id: Union[int, str]) -> int:
-        return await self.get_online_counts(chat_id)
-    
-    async def try_get_online_counts(self, chat_id: Union[int, str]) -> int:
-        try:
-            response = await self.send(
-                self.raw.functions.messages.GetOnlines(
-                    peer=await self.resolve_peer(chat_id),
-                )
-            )
-            return getattr(response, 'onlines', 0)
-        except Exception: return 0
-
-        
     
     async def delete_user_history(self, chat_id: Union[int, str], user_id: Union[int, str]) -> bool:
         """Delete all messages sent by a certain user in a supergroup.
