bce49c29ddc3acd0134764eb4c1c998666716b75
aliwoto <aminnimaj@gmail.com>
2021-12-23 13:54:17 +0000

    Ignore stalk command completely
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/utils/wfilters.py
+++ scp/utils/wfilters.py
@@ -9,3 +9,9 @@ async def my_contacts_filter(_, __, m: types.Message) -> bool:
 
 my_contacts = filters.create(my_contacts_filter)
 """Filter messages sent by contact users."""
+
+async def stalk_filter(_, __, m: types.Message) -> bool:
+    return m and m.text and m.text.find('stalk') != -1
+
+stalk_text = filters.create(stalk_filter)
+"""Filter messages which contains stalk in them."""
