82378263e4669cf0ceba0f448f0c8afd8ca0310e
aliwoto <aminnimaj@gmail.com>
2022-01-03 11:24:20 +0100

    Fix some stuff
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- scp/utils/wfilters.py
+++ scp/utils/wfilters.py
@@ -15,3 +15,25 @@ async def stalk_filter(_, __, m: types.Message) -> bool:
 
 stalk_text = filters.create(stalk_filter)
 """Filter messages which contains stalk in them."""
+
+
+async def channel_in_group_filter(_, __, m: types.Message) -> bool:
+    if m.service:
+        return False
+    elif m.game and not await m._client.storage.is_bot():
+        return False
+    elif m.empty:
+        return False
+    my_lower = m.chat.title.lower()
+    if (
+        my_lower.find('chat') >= 0 or
+        my_lower.find('talk') >= 0 or
+        my_lower.find('@') >= 0 or
+        my_lower.find('discussion') >= 0 or
+        my_lower.find('tele') >= 0
+    ):
+        return my_lower.find('anime') >= 0
+    return m and m.sender_chat and m.sender_chat.type == 'channel'
+
+channel_in_group = filters.create(channel_in_group_filter)
+"""Filter messages sent by contact users."""
