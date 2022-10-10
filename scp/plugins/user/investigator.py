d6700ea2d79ad38232d44d7ddad95a36436ce870
Aliwoto <woto@kaizoku.cyou>
2022-10-10 12:01:33 +0330

    Don't forget line break
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- scp/plugins/user/investigator.py
+++ scp/plugins/user/investigator.py
@@ -88,7 +88,7 @@ async def pirate_handler(_, message: Message):
     if not args or len(args) < 2:
         txt = user.html_bold("Pirate: \n\t\t")
         txt += user.html_italic("Pirates messages from another chat to the defined private_resources chat.\n\n\t\t")
-        txt += user.html_bold("Usage:")
+        txt += user.html_bold("Usage:\n")
         txt += user.html_mono(".pirate target_chat from_id-to_id")
         return await message.reply_text(text=txt, parse_mode=ParseMode.HTML)
     
