f226fa1111eed1a01725aedf84e2f36d6f94d23d
Aliwoto <aminnimaj@gmail.com>
2022-10-13 01:48:46 +0330

    Add try...except for command method and use format_exc function for logging the error details.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- scp/core/filters/Command.py
+++ scp/core/filters/Command.py
@@ -1,4 +1,6 @@
+import logging
 import re
+from traceback import format_exc
 from typing import List
 from pyrogram.filters import create
 from pyrogram.types import Message
@@ -27,32 +29,37 @@ def command(
     """
 
     async def func(flt, _: Client, message: Message):
-        text: str = message.text or message.caption
-        message.command = None
-        if not text:
+        try:
+            text: str = message.text or message.caption
+            message.command = None
+            if not text:
+                return False
+            regex = r'(?i)^({prefix})({regex})(@{bot_name})?(\s.*)?$'.format(
+                prefix='|'.join(re.escape(x) for x in flt.prefixes),
+                regex='|'.join(flt.commands),
+                bot_name=info['_user_username'].lower(),
+            )
+            if matches := re.search(regex, text, flags=re.IGNORECASE):
+                message.command = [matches.group(2)]
+                if matches.group(4):  # The command has arguments
+                    message.command.extend(
+                        [arg for arg in matches.group(4).strip().split()],
+                    )
+                return True
             return False
-        regex = r'(?i)^({prefix})({regex})(@{bot_name})?(\s.*)?$'.format(
-            prefix='|'.join(re.escape(x) for x in flt.prefixes),
-            regex='|'.join(flt.commands),
-            bot_name=info['_user_username'].lower(),
+        except Exception: logging.error(format_exc())
+    
+    try:
+        commands = commands if isinstance(commands, list) else [commands]
+        commands = {c if case_sensitive else c.lower() for c in commands}
+        prefixes = prefixes or []
+        prefixes = prefixes if isinstance(prefixes, list) else [prefixes]
+        prefixes = set(prefixes) if prefixes else {''}
+        return create(
+            func,
+            'CustomUserCommandFilter',
+            commands=commands,
+            prefixes=prefixes,
+            case_sensitive=case_sensitive,
         )
-        if matches := re.search(regex, text, flags=re.IGNORECASE):
-            message.command = [matches.group(2)]
-            if matches.group(4):  # The command has arguments
-                message.command.extend(
-                    [arg for arg in matches.group(4).strip().split()],
-                )
-            return True
-        return False
-    commands = commands if isinstance(commands, list) else [commands]
-    commands = {c if case_sensitive else c.lower() for c in commands}
-    prefixes = prefixes or []
-    prefixes = prefixes if isinstance(prefixes, list) else [prefixes]
-    prefixes = set(prefixes) if prefixes else {''}
-    return create(
-        func,
-        'CustomUserCommandFilter',
-        commands=commands,
-        prefixes=prefixes,
-        case_sensitive=case_sensitive,
-    )
+    except Exception: logging.error(format_exc())
