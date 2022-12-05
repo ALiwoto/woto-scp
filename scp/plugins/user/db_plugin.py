ec317f11434f8302f978508dc354414b7da42e65
Aliwoto <woto@kaizoku.cyou>
2022-12-06 01:27:23 +0330

    Add sql_table command.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- scp/plugins/user/db_plugin.py
+++ scp/plugins/user/db_plugin.py
@@ -3,6 +3,7 @@ from scp import user
 from pyrogram.types import Message
 import ujson
 
+
 @user.on_message(
 	user.filters.me &
 	user.filters.command(
@@ -25,3 +26,39 @@ async def sql_exec(_, message: Message):
     txt = user.html_mono(ujson.dumps(results, indent=4, ensure_ascii=False))
     return await message.reply_text(txt)
 
+@user.on_message(
+	user.filters.me &
+	user.filters.command(
+        ['sql_table'],
+        prefixes=user.cmd_prefixes,
+    ),
+)
+async def sql_table(_, message: Message):
+    table_name = user.get_non_cmd(message)
+    results = user.db.execute("""SELECT * FROM sqlite_schema
+                            WHERE type='table'
+                            ORDER BY name;
+                        """)
+    
+    if not table_name:
+        # fetch all table names and list them to the user.
+        all_tables = []
+        for current_result in results:
+            all_tables.append(current_result[1])
+        
+        txt = user.html_bold('Please specify a table name to see query of that table.\n')
+        txt += user.html_normal(f'there are currently {3} custom table(s) created:')
+        for current_name in all_tables:
+            txt += user.html_bold('\n1- ') + user.html_mono(current_name)
+        
+        return await message.reply_text(txt)
+    
+    table_cmd = ''
+    for current_result in results:
+        if current_result[1] == table_name:
+            table_cmd = current_result[4]
+    
+    if not table_cmd:
+        return await message.reply_text("couldn't find the specified table in database. \nmake sure the spelling is correct.")
+    
+    return await message.reply_text(user.html_mono(table_cmd))
