bb8c5421141abc95175debe4ea50ab6e01a96b95
Aliwoto <woto@kaizoku.cyou>
2022-12-06 00:13:22 +0330

    Upgrade some libraries to their latest version. Add DatabaseClient and user.db attribute. Add db_plugin.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

---
+++ scp/plugins/user/db_plugin.py
@@ -0,0 +1,22 @@
+
+from scp import user
+from pyrogram.types import Message
+import ujson
+
+@user.on_message(
+	user.filters.me &
+	user.filters.command(
+        ['sql'],
+        prefixes=user.cmd_prefixes,
+    ),
+)
+async def sql_exec(_, message: Message):
+    message.__str__
+    sql_raw = user.get_non_cmd(message)
+    results = user.db.execute(sql_raw)
+    if not results:
+        return await message.reply_text('No output.')
+
+    txt = user.html_mono(ujson.dump(results, indent=4, ensure_ascii=False))
+    return await message.reply_text(txt)
+
