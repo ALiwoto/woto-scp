92a12b43619de7dbb68d9c3dd53ef9c5e95e3c7a
Aliwoto <woto@kaizoku.cyou>
2023-04-06 14:04:23 +0330

    Fix config structure and add microsoft-azure credentials support.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- scp/woto_config.py
+++ scp/woto_config.py
@@ -1,77 +1,93 @@
 
 from configparser import ConfigParser
 import typing
+import logging
+
+
+def list_map(func, iterator):
+    return list(map(func, iterator))
 
 class WotoConfig:
     _the_config: ConfigParser = None
+
+    api_id: str = ''
+    api_hash: str = ''
+
     _sudo_users: typing.List[int] = []
     _owner_users: typing.List[int] = []
     _special_users: typing.List[int] = []
     prefixes: typing.List[str] = []
     log_channel: int = 0
-    api_id: str = ''
-    api_hash: str = ''
-    _enforcers = []
-    _inspectors = []
     dump_usernames = []
     private_resources: int = 0
     shared_channel: int = 0
+
+    _enforcers = []
+    _inspectors = []
     sibyl_token: int = 0
     public_listener: int = 0
     public_logger: int = 0
     private_listener: int = 0
     private_logger:int =0
+
     wp_username: str = ''
     wp_password: str = ''
     wp_host: str = 'wotoplatform.kaizoku.cyou'
     wp_port: int = 50100
 
-    def __init__(self) -> None:
+    _azure_sudo_users = []
+    azure_api_key: str = ''
+    azure_api_region: str = ''
+
+    def __init__(self, config_file='config.ini') -> None:
         self._the_config = ConfigParser()
-        self._the_config.read('config.ini')
+        self._the_config.read(config_file)
 
         self.api_id = self._the_config.get('pyrogram', 'api_id')
         self.api_hash = self._the_config.get('pyrogram', 'api_hash')
 
-        for x in self._the_config.get('woto-scp', 'OwnerList').split():
-            self._owner_users.append(int(x))
-        
-        for x in self._the_config.get('woto-scp', 'SudoList').split():
-            self._sudo_users.append(int(x))
-        
-        for x in self._the_config.get('woto-scp', 'special_users').split():
-            self._special_users.append(int(x))
+        self._owner_users = list_map(int, self._the_config.get('woto-scp', 'owner_list').split())
+        self._sudo_users = list_map(int, self._the_config.get('woto-scp', 'sudo_list').split())
+        self._special_users = list_map(int, self._the_config.get('woto-scp', 'special_users').split())
         
         self.prefixes = (
             self._the_config.get('woto-scp', 'Prefixes').split() or ['!', '.']
         )
-        self.log_channel = self._the_config.getint('woto-scp', 'LogChannel')
-
-        try:
-            for x in self._the_config.get('sibyl-system', 'enforcers').split():
-                self._enforcers.append(int(x))
-        except Exception as e:
-            print(e)
-        
-        try:
-            for x in self._the_config.get('sibyl-system', 'inspectors').split():
-                self._inspectors.append(int(x))
-        except Exception as e:
-            print(e)
-        
-        for x in self._the_config.get('woto-scp', 'public_dumps').split():
-            self.dump_usernames.append(x)
+        self.log_channel = self._the_config.getint('woto-scp', 'log_channel')
+        self.dump_usernames = list_map(int, self._the_config.get('woto-scp', 'public_dumps').split())
 
         self.private_resources = self._the_config.getint('woto-scp', 'private_resources')
         self.shared_channel = self._the_config.getint('woto-scp', 'shared_channel', fallback=self.private_resources)
-        # sibyl configuration stuff:
+
+        # sibyl configuration
+        try:
+            self.load_sibyl_config()
+        except Exception as e:
+            logging.warning(e, stacklevel=3)
+        
+        # woto-platform configuration
+        try:
+            self.load_wp_config()
+        except Exception as e:
+            logging.warning(e, stacklevel=3)
+
+        # microsoft-azure configuration
+        try:
+            self.load_azure_config()
+        except Exception as e:
+            logging.warning(e, stacklevel=3)
+        
+    
+    def load_sibyl_config(self) -> None:
+        self._special_users = list_map(int, self._the_config.get('sibyl-system', 'enforcers').split())
+        self._inspectors = list_map(int, self._the_config.get('sibyl-system', 'enforcers').split())
         self.sibyl_token = self._the_config.get('sibyl-system', 'token')
         self.public_listener = self._the_config.getint('sibyl-system', 'public_listener')
         self.public_logger = self._the_config.get('sibyl-system', 'public_logger')
         self.private_listener = self._the_config.get('sibyl-system', 'private_listener')
         self.private_logger = self._the_config.get('sibyl-system', 'private_logger')
-
-
+    
+    def load_wp_config(self) -> None:
         self.wp_username = self._the_config.get('woto-platform', 'username')
         self.wp_password = self._the_config.get('woto-platform', 'password')
         self.wp_host = self._the_config.get(
@@ -80,7 +96,13 @@ class WotoConfig:
             fallback='wotoplatform.kaizoku.cyou'
         )
         self.wp_port = self._the_config.getint('woto-platform', 'port', fallback=50100)
-    
+
+    def load_azure_config(self) -> None:
+        self._azure_sudo_users = list_map(
+            int, self._the_config.get('microsoft-azure', 'azure_sudo_users').split())
+        self.azure_api_key = self._the_config.get('microsoft-azure', 'azure_api_key')
+        self.azure_api_region = self._the_config.get('microsoft-azure', 'azure_api_region')
+
     def is_sudo(self, user: int) -> bool:
         return user in self._owner_users or user in self._sudo_users
     
