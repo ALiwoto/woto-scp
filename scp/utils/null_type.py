ba9b6e16c9b86c9ac56fee9f41db8fbc892d859f
Aliwoto <woto@kaizoku.cyou>
2023-06-11 15:55:48 +0330

    Fix nullable function.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- scp/utils/null_type.py
+++ scp/utils/null_type.py
@@ -1,15 +1,20 @@
 
 class _Null:
-  def __repr__(self):
-    return 'Null'
-  def __getattr__(self, _):
-    return self
-  def __eq__(self, other):
-    return other is None or isinstance(other, _Null)
-  def __bool__(self): return False
-  def __hash__(self): return hash(None)
+    def __repr__(self):
+        return 'Null'
+
+    def __getattr__(self, _):
+        return self
+
+    def __eq__(self, other):
+        return other is None or isinstance(other, _Null)
+
+    def __bool__(self): return False
+    def __hash__(self): return hash(None)
+
 
 Null = _Null()
+
 
 def nullable(cls):
     def nullable_func(func):
