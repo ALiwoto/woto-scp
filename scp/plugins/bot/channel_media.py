de1bcf2a85e6fd309930a2ecf736f4f305dfd51e
Aliwoto <aminnimaj@gmail.com>
2023-12-26 02:28:45 +0330

    Add support for album-format pixiv links.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- scp/plugins/bot/channel_media.py
+++ scp/plugins/bot/channel_media.py
@@ -8,14 +8,55 @@ from pyrogram.types import (
     ChatMemberUpdated, 
     ChatEventFilter,
     User,
-    Message
+    Message,
+    InputMediaPhoto,
+    InputMediaDocument,
 )
+from scp.utils.media_utils import PixivIllustInfo
 from urllib.parse import parse_qs, urlparse
 from pixivpy3 import AppPixivAPI
 
 pixiv_api = AppPixivAPI()
 setattr(user, 'pixiv_api', pixiv_api)
 setattr(bot, 'pixiv_api', pixiv_api)
+
+def do_pixiv_auth(pixiv_api: AppPixivAPI):
+    if not pixiv_api.access_token and user.scp_config.pixiv_access_token:
+        try:
+            pixiv_api.set_auth(
+                refresh_token=user.scp_config.pixiv_refresh_token,
+                access_token=user.scp_config.pixiv_access_token,
+            )
+            auth_result = pixiv_api.auth()
+
+            auth_date = datetime.datetime.now()
+            expires_in = auth_result.expires_in
+            setattr(pixiv_api, "auth_date", auth_date)
+            setattr(pixiv_api, "auth_expires_in", expires_in)
+            setattr(pixiv_api, "auth_expiration_date", 
+                    auth_date + datetime.timedelta(seconds=expires_in))
+        except: pass
+    elif pixiv_api.refresh_token:
+        # we know that we have already authorized, but we need
+        # to check if the authorization still valid or not
+        if getattr(pixiv_api, "auth_expiration_date", None) and \
+            datetime.datetime.now() < pixiv_api.auth_expiration_date:
+            # not expired yet
+            return
+        
+        # expired, so we need to re-auth, with recursive
+        pixiv_api.access_token = None
+        return do_pixiv_auth(pixiv_api)
+
+def has_single_meta_page_url(illust) -> bool:
+    try:
+        return bool(illust.illust.meta_single_page.original_image_url)
+    except: return False
+
+def has_multiple_meta_pages(illust) -> bool:
+    try:
+        return bool(illust.illust.meta_pages)
+    except: return False
 
 @bot.on_message(
     user.wfilters.pixiv &
@@ -32,14 +73,7 @@ async def pixiv_handler(_, message: Message):
     query_params = parse_qs(parsed_url.query)
     await message.delete()
 
-    if not pixiv_api.access_token and user.scp_config.pixiv_access_token:
-        try:
-            pixiv_api.set_auth(
-                refresh_token=user.scp_config.pixiv_refresh_token,
-                access_token=user.scp_config.pixiv_access_token,
-            )
-            pixiv_api.auth()
-        except: pass
+    do_pixiv_auth(pixiv_api)
     
     illust_id: str = None
     try:
@@ -59,46 +93,90 @@ async def pixiv_handler(_, message: Message):
     if not illust:
         return
     
-    # get the medium picture
-    file_path = f'pic_{illust_id}.jpg'
-    if not pixiv_api.download(illust.illust.image_urls.large, path='.', name=file_path):
-        return
-    
-    
+    illust_info = PixivIllustInfo(illust)
+    if illust_info.has_error:
+        do_pixiv_auth(pixiv_api)
+
+        # repeat it one more time
+        illust = pixiv_api.illust_detail(illust_id)
+        if not illust:
+            return
+        
+        illust_info = PixivIllustInfo(illust)
+        if illust_info.has_error:
+            # just give up at this point
+            print(f"failed to get illust info: {illust}")
+            return
     caption = user.html_link("Artist", url)
     caption += "\n@" + (user.html_normal(message.chat.username) \
         if message.chat.username \
         else (message.chat.usernames[0].username if message.chat.usernames else ""))
-    # send the picture
-    sent_photo_msg: Message = None
-    try:
-        sent_photo_msg = await bot.send_photo(
+    
+    if illust_info.is_multiple:
+        current_counter = 0
+        all_files_input = []
+        all_large_inputs = []
+        for current_meta in illust_info.meta_pages:
+            current_counter += 1
+            large_url = current_meta.image_urls.large
+            original_url = current_meta.image_urls.original
+            file_path = f'pic_{illust_id}_{current_counter}.jpg'
+            pixiv_api.download(large_url, path='.', name=file_path)
+            all_files_input.append(InputMediaPhoto(
+                media=file_path,
+                caption=(caption if current_counter == 1 else "")
+            ))
+            if len(all_files_input) >= 10:
+                break
+            
+            file_path = original_url.split('/')[-1]
+            pixiv_api.download(original_url, path='.', name=file_path)
+            all_large_inputs.append(InputMediaDocument(
+                media=file_path
+            ))
+            
+        sent_album = await bot.send_media_group(
             chat_id=message.chat.id, 
-            photo=file_path,
-            caption=caption
+            media=all_files_input
         )
-    except:
-        medium_file_path = f'pic_medium_{illust_id}.jpg'
-        if not pixiv_api.download(illust.illust.image_urls.medium, path='.', name=file_path):
-            return
-        await bot.send_photo(chat_id=message.chat.id, photo=file_path)
-        user.remove_file(medium_file_path)
+        await bot.send_media_group(
+            chat_id=message.chat.id, 
+            media=all_large_inputs,
+            reply_to_message_id=sent_album[0].message_id,
+        )
+    else:
+        # get the medium picture
+        file_path = f'pic_{illust_id}.jpg'
+        pixiv_api.download(illust.illust.image_urls.large, path='.', name=file_path)
+        
+        # send the picture
+        sent_photo_msg: Message = None
+        try:
+            sent_photo_msg = await bot.send_photo(
+                chat_id=message.chat.id, 
+                photo=file_path,
+                caption=caption
+            )
+        except:
+            medium_file_path = f'pic_medium_{illust_id}.jpg'
+            pixiv_api.download(illust.illust.image_urls.medium, path='.', name=file_path)
+            await bot.send_photo(chat_id=message.chat.id, photo=file_path)
+            user.remove_file(medium_file_path)
 
-    try:
-        if (illust.illust.meta_single_page.original_image_url):
+        try:
             original_url = illust.illust.meta_single_page.original_image_url
             file_original_path = original_url.split('/')[-1]
             if pixiv_api.download(original_url, path='.', name=file_original_path):
                 user.remove_file(file_path)
                 file_path = file_original_path
-    except: pass
+        except: pass
 
-    await bot.send_document(
-        chat_id=message.chat.id,
-        document=file_path,
-        reply_to_message_id=sent_photo_msg.message_id,
-    )
-    user.remove_file(file_path)
+        await bot.send_document(
+            chat_id=message.chat.id,
+            document=file_path,
+            reply_to_message_id=sent_photo_msg.message_id,
+        )
+        user.remove_file(file_path)
 
     
 
