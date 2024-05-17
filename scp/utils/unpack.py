e874489f3f73554850fb2a00a2afa6be13814fa0
Aliwoto <aminnimaj@gmail.com>
2024-05-17 20:49:03 +0330

    Fix message parsing and put some delays here and there.
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- scp/utils/unpack.py
+++ scp/utils/unpack.py
@@ -1,9 +1,21 @@
 import base64
 import struct
 from attrify import Attrify as Atr
+from pyrogram.types import Message
+from pyrogram.raw.types.web_view_result_url import WebViewResultUrl
 
 
-def unpackInlineMessage(inline_message_id: str) -> Atr:
+class WebViewResultContainer(WebViewResultUrl):
+
+    # the message this web view result belong to.
+    message: Message = None
+
+    def __init__(self, *, query_id: int, url: str, message: Message) -> None:
+        super().__init__(query_id=query_id, url=url)
+        self.message = message
+    
+
+def unpack_inline_message(inline_message_id: str) -> Atr:
     temp = {}
     dc_id, message_id, chat_id, query_id = struct.unpack(
         '<iiiq',
