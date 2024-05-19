293381fe06d572479e3afedc41c509ec76d72f25
Aliwoto <aminnimaj@gmail.com>
2024-05-20 01:35:02 +0330

    Fix formatting issue.
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- scp/plugins/user/start.py
+++ scp/plugins/user/start.py
@@ -22,18 +22,19 @@ async def _(_, message: Message):
         'SELECT COUNT(id) FROM peers WHERE type in ("user", "bot")',
     )
 
+    TAB = "\t\t\t\t"
     text = user.html_bold("woto-scp\n")
     text += user.html_bold("\t\tversion: ")
     text += user.html_link(__version__,
                            f"https://github.com/ALiwoto/woto-scp/commit/{__long_version__}")
-    text += user.html_bold("\t\t\t\tdc_id: ") + user.html_mono(await user.storage.dc_id())
-    text += user.html_bold("\t\t\t\tping_dc: ") + \
+    text += user.html_bold(f"\n{TAB}dc_id: ") + user.html_mono(await user.storage.dc_id())
+    text += user.html_bold(f"\n{TAB}ping_dc: ") + \
         user.html_mono(f'{round((end - start) * 1000, 3)}ms')
-    text += user.html_bold("\t\t\t\tpeer_users: ") + \
+    text += user.html_bold(f"\n{TAB}peer_users: ") + \
         user.html_mono(f'{users_query_result[0][0]} users')
-    text += user.html_bold("\t\t\t\tpeer_groups: ") + \
+    text += user.html_bold(f"\n{TAB}peer_groups: ") + \
         user.html_mono(f'{groups_query_result[0][0]} groups')
-    text += user.html_bold("\t\t\t\tscp_uptime: ") + \
+    text += user.html_bold(f"\n{TAB}scp_uptime: ") + \
         user.html_mono(humanize_time(time.time() - RUNTIME))
     buttons = [
         {"Source": "https://github.com/aliwoto/woto-scp", "close": "close_message"},
